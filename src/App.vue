dfd6e584112f78edd086aefd3d856c8bc65f6381
luthfikhaerii <luthfikhaeri00@gmail.com>
2025-09-14 17:31:46 +0700

    update

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@ import Sidebar from './components/Sidebar.vue';
 </script>
 
 <template>
-  <div class="flex">
+  <div class="flex min-h-screen">
     <Sidebar/>
   </div>
   <p class="text-coklat">hallo world</p>
--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,7 +1,11 @@
 <template>
-    <div class="w-64">
-        <di><p>Artikel</p></di>
-        <di><p>Trip</p></di>
-        <di><p>Registration</p></di>
+    <div class="w-64 bg-red-200">
+        <p class="text-2xl font-bold mt-10 ml-6">Langakahsana</p>
+        <div class="w-full">
+        <di class="px-4 py-2 text-gray-400"><p>Artikel</p></di>
+        <di class="px-4 py-2 text-gray-400"><p>Artikel</p></di>
+        <di class="px-4 py-2 text-gray-400"><p>Artikel</p></di>
+        </div>
+
     </div>
 </template>
